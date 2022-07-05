ce20ffa93c1efe3deb9efae13aaaf0a4ca0b3139
Adeoti Shuaib <Adeotishuaib2001@gmail.com>
2022-07-05 12:21:45 +0100

    Stack implementation

---
+++ Stack/Stack.js
@@ -0,0 +1,26 @@
+class Stack {
+  constructor() {
+    this.dataStore = [];
+    this.top = 0;
+    this.pop = pop;
+    this.push = push;
+    this.peek = peek;
+    this.length = length;
+    this.clear = clear;
+  }
+}
+function push(element) {
+  this.dataStore[this.top++] = element;
+}
+function pop() {
+  return this.dataStore[--this.top];
+}
+function peek() {
+  return this.dataStore[this.top - 1];
+}
+function length() {
+  return this.top;
+}
+function clear() {
+  this.top = 0;
+}
